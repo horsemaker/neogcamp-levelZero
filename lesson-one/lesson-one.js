e0ffcc6efa8c259a5cefb9149bf37a1861ce01ab
horsemaker <yash_ghodekar@yahoo.in>
2021-06-17 16:38:08 +0530

    Done till ex07

--- lesson-one/lesson-one.js
+++ lesson-one/lesson-one.js
@@ -7,7 +7,7 @@ prompt('Give me your name: ') // On Browser
 
 var readlineSync = require('readline-sync')
 var userName = readlineSync.question('May I have your name?')
-console.log(userName);
+console.log(userName)
 // ex02 - end
 
 // ex03 - start
@@ -23,66 +23,66 @@ console.log(welcomeMessage)
 // ex04 - end
 
 // ex05 - start
-var readlineSync = require('readline-sync');
+var readlineSync = require('readline-sync')
 // input
-var userAnswerAge = readlineSync.question("Am I older than 25? ");
-console.log("You entered " + userAnswerAge)
+var userAnswerAge = readlineSync.question('Am I older than 25? ')
+console.log('You entered ' + userAnswerAge)
 
 // processing
-if (userAnswerAge === "yes") {
+if (userAnswerAge === 'yes') {
   // output
-  console.log("You are wrong!")
+  console.log('You are wrong!')
 } else {
   // output
-  console.log("You are right!")
+  console.log('You are right!')
 }
 // ex05 -end
 
 // ex06 - start
-var readlineSync = require('readline-sync');
+var readlineSync = require('readline-sync')
 
-var score = 0;
+var score = 0
 
 // question 1
-var questionOne = "Am I older than 18? ";
-var answerOne = "yes";
+var questionOne = 'Am I older than 18? '
+var answerOne = 'yes'
 
 // input
-var userAnswerAge = readlineSync.question(questionOne);
-console.log("You entered " + userAnswerAge)
+var userAnswerAge = readlineSync.question(questionOne)
+console.log('You entered ' + userAnswerAge)
 
 // processing
 if (userAnswerAge === answerOne) {
   // output
-  console.log("You are right!")
-  score = score + 1;
-  console.log("Score is: " + score)
+  console.log('You are right!')
+  score = score + 1
+  console.log('Score is: ' + score)
 } else {
   // output
-  console.log("You are wrong!")
-  score = score - 1;
-  console.log("Score is: " + score)
+  console.log('You are wrong!')
+  score = score - 1
+  console.log('Score is: ' + score)
 }
 
 // question 2
-var questionTwo = "Are you a boy? ";
-var answerTwo = "yes";
+var questionTwo = 'Are you a boy? '
+var answerTwo = 'yes'
 
 // input
-var userAnswerGender = readlineSync.question(questionTwo);
-console.log("You entered " + userAnswerGender)
+var userAnswerGender = readlineSync.question(questionTwo)
+console.log('You entered ' + userAnswerGender)
 
 // processing
 if (userAnswerAge === answerTwo) {
   // output
-  console.log("You are right!")
-  score = score + 1;
-  console.log("Score is: " + score)
+  console.log('You are right!')
+  score = score + 1
+  console.log('Score is: ' + score)
 } else {
   // output
-  console.log("You are wrong!")
-  score = score - 1;
-  console.log("Score is: " + score)
+  console.log('You are wrong!')
+  score = score - 1
+  console.log('Score is: ' + score)
 }
 // ex06 - end
 
@@ -91,25 +91,27 @@ if (userAnswerAge === answerTwo) {
 
 // function to add two numbers
 function add(numberOne, numberTwo) {
-    console.log("numberOne: ", numberOne);
-    console.log("numberTwo: ", numberTwo);
-    var sum = numberOne + numberTwo;
-    console.log("The sum of " + numberOne + " and " + numberTwo + " is : " + sum);
-  }
-  
-  // use the function
-  add(2, 5);
-  
-  // function to find product of two numbers
-  function product(numberOne, numberTwo) {
-    console.log("numberOne: ", numberOne);
-    console.log("numberTwo: ", numberTwo);
-    var product = numberOne * numberTwo;
-    console.log("The product of " + numberOne + " and " + numberTwo + " is : " + product);
-  }
-  
-  // use the function
-  product(6, 7);
+  console.log('numberOne: ', numberOne)
+  console.log('numberTwo: ', numberTwo)
+  var sum = numberOne + numberTwo
+  console.log('The sum of ' + numberOne + ' and ' + numberTwo + ' is : ' + sum)
+}
+
+// use the function
+add(2, 5)
+
+// function to find product of two numbers
+function product(numberOne, numberTwo) {
+  console.log('numberOne: ', numberOne)
+  console.log('numberTwo: ', numberTwo)
+  var product = numberOne * numberTwo
+  console.log(
+    'The product of ' + numberOne + ' and ' + numberTwo + ' is : ' + product
+  )
+}
+
+// use the function
+product(6, 7)
 // ex07 - end
 
 // ex08 - start
@@ -118,6 +120,9 @@ function add(numberOne, numberTwo) {
 
 // ex09 - start
 // ex09 - end
+
+// homework - start
+// homework - end
 
 // ex10 - start
 // ex10 - end
